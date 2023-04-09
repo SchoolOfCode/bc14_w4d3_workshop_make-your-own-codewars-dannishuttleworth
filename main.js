d4d12fed67e0e6caf23120e2b463d5a164f47ec7
dannishuttleworth <124736568+dannishuttleworth@users.noreply.github.com>
2023-04-09 10:19:05 +0100

    Initial scenario and test creation

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 /* 
 👉 Write your kata here!
+
+We all know the infamous King Henry VIII and his six wives. But when you sit a history quiz, their
+names and fate simply escape your mind!
+
+Create a function which will enable you to return the correct wife and correct fate when their number (1-6)
+is entered into the function, in the following format:
+
+Wife number: <wifeNumber>, Wife name: <wifeName>, Wife fate: <wifeFate>
+
+1 - Catherine of Aragon (Divorced)
+2 - Anne Boleyn (Beheaded)
+3 - Jane Seymour (Died)
+4 - Anne of Cleves (Divorced)
+5 - Katherine Howard (Beheaded)
+6 - Katherine Parr (Survived)
+
+For function arguments other than 1-6, the function should return the message "Not a number between 1-6, try again" 
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function whichWife (wifeNumber){
+    const allWives = ["Catherine of Aragon", "Anne Boleyn", "Jane Seymour", "Anne of Cleves", "Katherine Howard", "Katherine Parr"];
+    const fate = ["Divorced", "Beheaded", "Died", "Divorced", "Beheaded", "Survived"];
+    if (wifeNumber <= 6){
+        return `Wife Number: ${wifeNumber}, Wife name: ${allWives[wifeNumber-1]}, Wife fate: ${fate[wifeNumber-1]}`
+    }
+    else {
+        return `Not a number between 1-6, try again`;
+    }
+        
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,45 @@
 //👉 Write your tests below here:
+import {test, expect} from "@jest/globals";
+import {whichWife} from "./main.js";
+
+test("should return 5 Katherine Howard Beheaded", () => {
+const actual = whichWife(5);
+const expected = "Wife number: 5, Wife name: Katherine Howard, Wife fate: Beheaded";
+expect(actual),toBe(expected);
+});
+
+test("should return 2 Anne Boleyn Beheaded", () => {
+    const actual = whichWife(2);
+    const expected = "Wife number: 2, Wife name: Anne Boleyn, Wife fate: Beheaded";
+    expect(actual),toBe(expected);
+    });
+
+test("should return 3 Jane Seymour Died", () => {
+    const actual = whichWife(3);
+    const expected = "Wife number: 3, Wife name: Jane Seymour, Wife fate: Died";
+    expect(actual),toBe(expected);
+    });
+
+test("should return 4 Anne of Cleves Divorced", () => {
+    const actual = whichWife(4);
+    const expected = "Wife number: 4, Wife name: Anne of Cleves, Wife fate: Divorced";
+    expect(actual),toBe(expected);
+    });
+
+test("should return 6 Katherine Parr Survived", () => {
+    const actual = whichWife(6);
+    const expected = "Wife number: 6, Wife name: Katherine Parr, Wife fate: Survived";
+    expect(actual),toBe(expected);
+    });
+
+test("should return 1 Catherine of Aragon Divorced", () => {
+    const actual = whichWife(1);
+    const expected = "Wife number: 1, Wife name: Catherine of Aragon, Wife fate: Divorced";
+    expect(actual),toBe(expected);
+    });
+
+test("should return Try again 1-6 only"), () => {
+    const actual = whichWife(18);
+    const expected = "Not a number between 1-6, try again";
+    expect(actual).toBe(expected);
+}
